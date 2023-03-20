4663cbb55eb27dfd8994cc6b505ce972bdaf11db
Arpan Sarkar <asarkar@vectra.ai>
2023-03-20 15:10:59 -0700

    Added input checks and code reuse

--- Library/BruteForce.ps1
+++ Library/BruteForce.ps1
@@ -1,23 +1,30 @@
-function BruteForce {
-
-    mitre_details("BruteForce")
-
-    #Get account to target
-    EnterAccount ("nEnter an account to brute-force (eg:user@org.com) or enter 'recon' to find all available accounts")
-    $username = $global:input_user_account
-
+###Primary Bruteforce function
+function BruteForce ($username){
     #Check file input
     $check_file = $false
     while ($check_file -eq $false) {
-        $filename = Read-Host -Prompt "`nEnter the password dictionary text file name (include file extension)"
+        #Display available files in MAAD-AF directory
+        $file_list = Get-ChildItem -Path ./* -Include *.txt
+        if ($null -eq $file_list) {
+            Write-Host "`nNote: No potential dictionary files found. Add a password dictionary text file to '$((Get-Item ./).FullName)'" -ForegroundColor Red
+        }
+        else {
+            Write-Host "`nPossible dictionary files found in the MAAD-AF directory:" -ForegroundColor Gray
+            foreach ($file in $file_list.Name){
+                Write-Host "- $file" -ForegroundColor Gray
+            }
+        }
+
+        $filename = Read-Host -Prompt "`nEnter the password dictionary file (include file extension)"
+        $filename = $filename.Trim()
         $check_file = Test-Path -Path .\$filename
         
-        if ($check_file) {
+        if ($check_file -and $filename -ne "") {
             Write-Host "File found!!!"
             #Check file format - Only txt files accepted
             $extn = [IO.Path]::GetExtension($filename) 
             if ($extn -ne ".txt") {
-                Write-Host "`nInvalid file type: Please provide a 'txt' dictionary file with each password on a new line."
+                Write-Host "`nInvalid file type: Please provide a 'txt' dictionary file with each password on a new line." -ForegroundColor Red
                 $check_file = $false
             }
             else {
@@ -25,7 +32,8 @@ function BruteForce {
             } 
         }
         else {
-            Write-Host "`nPassword file: "$filename" not found. Check -`n1.If the spelling is correct`n2.If the file exists in the root directory of MAAD`n3.Include extension in filename"
+            Write-Host "`nPassword file: '$filename' not found. Check -`n1.If the spelling is correct`n2.If the file exists in the same directory as the tool`n3.Include extension in filename" -ForegroundColor Red
+            $check_file = $false
         }
     }
     
@@ -55,8 +63,9 @@ function BruteForce {
             
             Write-Host "`nSuccess is No Accident ;) Successfully cracked account password!!!" -ForegroundColor Yellow -BackgroundColor Black
             $userobject | Format-Table 
-            $userobject | Out-File -FilePath .\Outputs\Internal_BruteForce_Result.txt
-            break 
+            $userobject | Out-File -FilePath .\Outputs\External_BruteForce_Result.txt
+            break
+
         } 
         catch {
             $counter++
@@ -68,4 +77,27 @@ function BruteForce {
         Write-Host "`nBrute-force Unsuccessful!!! Try another password dictionary or account!!!" -ForegroundColor Yellow -BackgroundColor Black
     }
     Pause
+}
+
+###Internal Bruteforce function
+function InternalBruteForce {
+    mitre_details("BruteForce")
+
+    #Get account to target
+    EnterAccount ("`nEnter an account to brute-force (eg:user@org.com) or enter 'recon' to find all available accounts")
+    $username = $global:input_user_account
+
+    BruteForce($username)
+}
+
+###External Bruteforce function
+function ExternalBruteForce {
+    mitre_details("BruteForce")
+
+    #Get account to target
+    do {
+        $username = Read-Host -Prompt "`nEnter an account to brute-force (eg:user@org.com)"
+    } until ("" -ne $username)
+
+    BruteForce($username)
 }

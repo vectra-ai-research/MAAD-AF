e213c3915d5b0027faa3d63823b32d9b1f606eef
Arpan Sarkar <asarkar@vectra.ai>
2023-04-04 16:24:44 -0700

    Added support to store multiple credentials

--- Library/MAAD_Config.ps1
+++ Library/MAAD_Config.ps1
@@ -3,24 +3,32 @@
     This file maintains global config for various MAAD-AF modules. 
 
 .NOTES
-    Configurations in this file are optional and not required for primary operation of MAAD-AF modules with the exception of TOR module. 
+    Configurations in this file are optional and not required for primary operation of MAAD-AF with the exception of TOR module. 
+    1. Add credentials to $CredentialList if you would like to reuse them often. You can add multiple credntials and you can choose one directly for access in MAAD-AF.
+    Access token in credentials is optional (if they are provided, MAAD-AF will default to establishing access with them first)
+
+    2. Edit the TOR root folder path in the TOR configuration section (other defaults in this config can remain default) if you would like to route MAAD-AF traffic through TOR. 
+    Make sure you have TOR installed in your host. If not, you need to install it manually to use MAAD TOR module
+
+.EXAMPLE
+Add credntials to $CredentialList in the following format:
+$global:CredentialsList = @(@{"username" = "user1@foo.com";"password" = "xyz_zyx";"token"= "123abc"},@{"username" = "user2@foo.com";"password" = "xyz_zyx";"token"=""})
 #>
 
 #####Access credentials#####
 
-#Note: This configuration is totally optional. Set the crompromised credentials here if you wish to not enter them at run time.
-$global:AdminUsername = "Enter_Username_Here@domain.com"
-$global:AdminPassword = "Enter_Password_Here!"
+#Note: This configuration is totally optional. Add the crompromised credentials to the if you wish to the $global:CredentialsList list below.
 
+$global:CredentialsList = @()
 
 #####TOR Configuration#####
 
-#Note: Make sure you have TOR installed in your host. If not, you need to install it manually to use MAAD TOR module
+
 
 #Enter TOR root folder path
 $global:tor_root_directory = "C:\Users\username\sub_folder\Tor Browser"
 
-#The below TORconfiguration does not need to be modified unless you would like to or are facing any issues
+#The below TOR configuration does not need to be modified unless you would like to or if you are facing any issues
 $global:tor_host = "127.0.0.1"  
 #Port used by TOR          
 $global:tor_port = 9150
